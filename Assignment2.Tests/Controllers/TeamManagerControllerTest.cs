8c21afe9efca75b6caab2bca2c90ea6bb486363e
JCannon1 <jessecannon1@hotmail.com>
2017-12-17 14:43:52 -0500

    Edited the tests

--- Assignment2.Tests/Controllers/TeamManagerControllerTest.cs
+++ Assignment2.Tests/Controllers/TeamManagerControllerTest.cs
@@ -12,12 +12,12 @@ using System.Linq;
 namespace Assignment2.Tests.Controllers
 {
     [TestClass]
-    public class TeamManagerControllerTest
+    public class TeamManagerControllerTest 
     {
         // Globals 
-        TeamController controler;
+        HockeyTeamsController controller;
         Mock<ITeamManagerRepository> mock;
-        List<Team> teams;
+        List<Table_HockeyTeams> teams;
 
         [TestInitialize]
         public void TestInitialize()
@@ -26,18 +26,18 @@ namespace Assignment2.Tests.Controllers
             mock = new Mock<ITeamManagerRepository>();
 
             // Mock Team data
-            teams = new List<Team>
+            teams = new List<Table_HockeyTeams>
             {
-                new Team { TeamId = 4, Title = "Team 4", City = "Florida" },
-                new Team { TeamId = 5, Title = "Team 5", City = "New Jersey" },
-                new Team { TeamId = 6, Title = "Team 6", City = "North Carolina" }
+                new Table_HockeyTeams { teamId = "4", teamName = "Team 4", teamCity = "Florida" },
+                new Table_HockeyTeams { teamId = "5", teamName = "Team 5", teamCity = "New Jersey" },
+                new Table_HockeyTeams { teamId = "6", teamName = "Team 6", teamCity = "North Carolina" }
             };
 
             // Add Team data to the mock object
             mock.Setup(m => m.Teams).Returns(teams.AsQueryable());
 
             // Pass the mock to the controller
-            controller = new TeamController(mock.Object);
+            controller = new HockeyTeamsController(mock.Object);
         }
 
         // 1. Test to see if Index loads the correct View 
@@ -59,7 +59,7 @@ namespace Assignment2.Tests.Controllers
         public void IndexShowsValidTeams()
         {
             // Act
-            var actual = (List<Team>)Controller.Index().Model;
+            var actual = (List<Table_HockeyTeams>)controller.Index().Model;
 
             // Assert
             CollectionAssert.AreEqual(teams, actual);
@@ -70,43 +70,43 @@ namespace Assignment2.Tests.Controllers
         public void DetailsValidId()
         {
             // Act
-            
+            var actual = (Table_HockeyTeams)controller.Details("4").Model;
 
             // Assert
-           
+            Assert.AreEqual(teams.ToList()[0], actual);
         }
 
         // 4. Test to see if Details loads the error view with an invalid Id
         [TestMethod]
-        public void DetailsErrorView()
+        public void DetailsInvalidId()
         {
             // Act
-            
+            ViewResult actual = controller.Details("4");
 
             // Assert
-           
+            Assert.AreEqual("Error", actual.ViewName);
         }
 
         // 5. Test to see if Details loads the error view with no Id 
         [TestMethod]
-        public void DetailsErrorViewNoId()
+        public void DetailsInvalidNoId()
         {
             // Act
-
+            ViewResult actual = controller.Details(null);
 
             // Assert
-
+            Assert.AreEqual("Error", actual.ViewName);
         }
 
         // 6. Test to see if DeleteConfirmed deletes the correct object 
         [TestMethod]
-        public void DeleteConfirmed()
+        public void DeleteConfirmedNoId()
         {
             // Act
-
+            ViewResult actual = controller.DeleteConfirmed(null);
 
             // Assert
-
+            Assert.AreEqual("Error", actual.ViewName);
         }
 
         // 7. Test to see if DeleteConfirmed loads the error view with an invalid Id
@@ -114,21 +114,22 @@ namespace Assignment2.Tests.Controllers
         public void DeleteConfirmedInvalidId()
         {
             // Act
-
+            ViewResult actual = controller.DeleteConfirmed("4");
 
             // Assert
+            Assert.AreEqual("Error", actual.ViewName);
 
         }
 
-        // 8. Test to see if DeleteConfirmed loads the error view with no Id
-        [TestMethod]
-        public void DeleteConfirmedNoId()
+        // 8. Test to see if DeleteConfirmed loads the error view with valid Id
+        [TestMethod] 
+        public void DeleteConfirmedValidId()
         {
             // Act
-
+            ViewResult actual = controller.DeleteConfirmed("1");
 
             // Assert
-
+            Assert.AreEqual("Error", actual.ViewName);
         }
     }
 }
